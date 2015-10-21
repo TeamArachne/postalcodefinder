dbf3920001612ec7a81a5e10d477bf2f943d4086
martincostello <martin@martincostello.com>
2015-10-21 21:13:46 +0100

    freegeoip.net client integration. A small client library and accompanying test harness to use the https://freegeoip.net/json/ API.

---
+++ freegeoip.client/FreeGeoIP.Client.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FreeGeoIP.Client.Console")]
+[assembly: AssemblyDescription("Console application for FreeGeoIP.Client.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Experian")]
+[assembly: AssemblyProduct("EDQ HackDay 2015")]
+[assembly: AssemblyCopyright("Copyright © Experian, 2015. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30f9d2e8-0a09-4aee-ac58-e0373194cd63")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ freegeoip.client/FreeGeoIP.Client/IPLookup.cs
@@ -0,0 +1,76 @@
+﻿namespace FreeGeoIP.Client
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// A class representing the result of a <c>https://freegeoip.net</c> IP lookup. This class cannot be inherited.
+    /// </summary>
+    public sealed class IPLookup
+    {
+        /// <summary>
+        /// Gets or sets the matched IP address.
+        /// </summary>
+        [JsonProperty("ip")]
+        public string IPAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country code associated with the IP address.
+        /// </summary>
+        [JsonProperty("country_code")]
+        public string CountryCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country name associated with the IP address.
+        /// </summary>
+        [JsonProperty("country_name")]
+        public string CountryName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the region code associated with the IP address.
+        /// </summary>
+        [JsonProperty("region_code")]
+        public string RegionCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the region name associated with the IP address.
+        /// </summary>
+        [JsonProperty("region_name")]
+        public string RegionName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the city associated with the IP address.
+        /// </summary>
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        /// <summary>
+        /// Gets or sets the postal code associated with the IP address.
+        /// </summary>
+        [JsonProperty("zip_code")]
+        public string PostalCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time zone associated with the IP address.
+        /// </summary>
+        [JsonProperty("time_zone")]
+        public string TimeZone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the latitude associated with the IP address.
+        /// </summary>
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the longitude associated with the IP address.
+        /// </summary>
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the metro code associated with the IP address.
+        /// </summary>
+        [JsonProperty("metro_code")]
+        public double MetroCode { get; set; }
+    }
+}
