b4e9f417953fd0462663a75a9b8c13e4820aaa9b
M15sy <misayocurtis@hotmail.co.uk>
2015-10-22 09:39:36 +0100

    Add API project.

---
+++ postalcodefinder/postalcodefinder/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace postalcodefinder.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public object Post([FromBody]string value)
+        {
+            return new
+            {
+                iso = "GB",
+                postalCode = "SW11",
+                method = "Coords"
+            };
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ postalcodefinder/postalcodefinder/PcfGlobal.cs
@@ -0,0 +1,15 @@
+﻿namespace postalcodefinder
+{
+    using postalcodefinder.Configuration;
+
+    public static class PcfGlobal
+    {
+        private static IPcfConfigurationSection _config = PcfConfigurationSection.GetSection();
+
+        public static IPcfConfigurationSection Configuration
+        {
+            get { return _config; }
+            internal set { _config = value ?? PcfConfigurationSection.GetSection(); }   // Ensure never null
+        }
+    }
+}
